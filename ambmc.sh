861af2769d8151b36f9a90d2d14e1128676a513b
setina42 <tina.seg@bluewin.ch>
2018-11-07 12:41:21 +0000

    mesh genererate

--- ambmc.sh
+++ ambmc.sh
@@ -42,7 +42,7 @@ fslswapdim _ambmc_200micron.nii x -y z ambmc_200micron.nii
 fslorient -setsform 0.2 0 0 -4.924 0 0.2 0 -9.5855 0 0 0.2 -3.726 0 0 0 1 ambmc_200micron.nii.gz
 fslorient -copysform2qform ambmc_200micron.nii.gz
 
-# Make Mask
+# Make Masks
 fslmaths ambmc_200micron.nii.gz -thr 10 -bin ambmc_200micron_mask.nii.gz
 fslmaths ambmc_40micron.nii.gz -thr 10 -bin ambmc_40micron_mask.nii.gz
 fslmaths ambmc_15micron.nii.gz -thr 10 -bin ambmc_15micron_mask.nii.gz
