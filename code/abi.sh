f6bc91b0f6af95b33f480fed1e8e0410c51c8daa
Horea Christian <chr@chymera.eu>
2023-05-13 13:34:17 -0400

    No fail on existing direcotry

--- code/abi.sh
+++ code/abi.sh
@@ -9,7 +9,7 @@ WDIR="work/"
 get_resource "http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/average_template/average_template_10.nrrd"
 get_resource "http://download.alleninstitute.org/informatics-archive/current-release/mouse_ccf/annotation/ccf_2017/annotation_10.nrrd"
 
-mkdir "${WDIR}"
+mkdir -p "${WDIR}"
 
 cp "${RDIR}/average_template_10.nrrd" "${WDIR}/abi_10micron_average.nrrd"
 cp "${RDIR}/annotation_10.nrrd" "${WDIR}/abi_10micron_annotation.nrrd"
